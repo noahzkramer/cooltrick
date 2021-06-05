58e7eed5e0942f037c644729c57eca0fbecd7746
Jamie Pask <jamiepask1392@gmail.com>
2021-06-04 18:41:16 -0700

    fix bg-med

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 const plugin = require('tailwindcss/plugin')
 
-const num = ['med']
-const whitelist = []
-num.map((x) => { whitelist.push('bg-' + x) })
-
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     options: {
-      whitelist,
+      safelist: ['bg-med']
     }
   },
   darkMode: false, // or 'media' or 'class'
