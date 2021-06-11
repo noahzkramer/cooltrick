2e6ab6d6403c8189cf5585e00d8cb7c214dde56d
“jpask1392git config --global user.email “jamiepask1392@gmail.com <noahzkramer@gmail.com>
2021-06-10 17:37:54 -0700

    update preivew

--- components/global/Alert.jsx
+++ components/global/Alert.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import styled from 'styled-components'
 
-const Alert = () => {
+const Alert = ({className}) => {
   const [ showBar, setShowBar ] = useState(true)
 
   return (
     showBar && (
-      <div className="alert">
+      <div className={className}>
         This is a preview page.{' '}
         <a href="/api/exit-preview"> Click here</a>{' '}
         to exit preview mode.{' '}
@@ -15,4 +16,20 @@ const Alert = () => {
   )
 }
 
-export default Alert
+export default styled(Alert)`
+  font-size: 1.5rem;
+  line-height: 2.8rem;
+  padding: 1.6rem;
+  background-color: rgba(32, 33, 35, 1);
+  color: white;
+  position: fixed;
+  border-radius: 8px;
+  font-weight: 300;
+  bottom: 15px;
+  right: 15px;
+  z-index: 1060;
+
+  a {
+    color: white;
+  }
+`
